29af5ad0a415ab852fb275094bc510970765f43b
Mathis Pandelé <mapandel@student.42.fr>
2017-10-03 12:42:33 +0200

    rostring.php 1.0 : Implementation

---
+++ Day01/ex07/rostring.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?php
+	if (!($argc > 1) || !$argv[1])
+		return (NULL);
+	$array = explode(" ", $argv[1]);
+	$array = array_filter($array);
+	$str = array_shift($array);
+	foreach ($array as $tmp)
+		echo $tmp . " ";
+	if ($str)
+		echo $str . "\n";
+?>
