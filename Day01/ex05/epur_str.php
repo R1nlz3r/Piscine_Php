740e50c4b9b383b9d02a5c153813a1a8cc679a13
Mathis Pandelé <mapandel@student.42.fr>
2017-10-04 18:23:02 +0200

    epur_str.php 1.1 : Optimisation

--- Day01/ex05/epur_str.php
+++ Day01/ex05/epur_str.php
@@ -3,8 +3,7 @@
 	if ($argc != 2)
 		return (NULL);
 	$str = preg_replace('/\s\s+/', ' ', $argv[1]);
-	$str = ltrim($str);
-	$str = rtrim($str);
+	$str = trim($str);
 	if ($str)
 		echo  $str . "\n";
 ?>
