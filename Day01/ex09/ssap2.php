b77863a5a8f81ba083b4d590309ebf0c6f8126cb
Mathis PANDELE <mapandel@e2r3p10.42.fr>
2017-10-04 21:47:57 +0200

    ssap2.php 1.1 : Fix

--- Day01/ex09/ssap2.php
+++ Day01/ex09/ssap2.php
@@ -2,24 +2,21 @@
 <?php
 	function ft_split($str)
 	{
-		if (!$str)
-			return (NULL);
 		$array = explode(" ", $str);
-		$array = array_filter($array);
+		$array = array_filter($array, 'strlen');
+		sort($array, SORT_STRING);
 		return ($array);
 	}
 
 	$i = 1;
 	while ($i < $argc)
 	{
-		if ($array)
+		if ($i != 1)
 			$array = array_merge($array, ft_split($argv[$i]));
 		else
 			$array = ft_split($argv[$i]);
 		++$i;
 	}
-	if (!$array)
-		return (NULL);
 	foreach ($array as $tmp)
 	{
 		if (ctype_alpha($tmp))
@@ -29,13 +26,23 @@
 		else
 			$rest[] = $tmp;
 	}
-	sort($alpha, SORT_FLAG_CASE | SORT_STRING);
-	foreach ($alpha as $tmp)
-		echo $tmp . "\n";
-	sort($nb, SORT_STRING);
-	foreach ($nb as $tmp)
-		echo $tmp . "\n";
-	sort($rest, SORT_STRING);
-	foreach ($rest as $tmp)
-		echo $tmp . "\n";
+
+	if (!(empty($alpha)))
+	{
+		sort($alpha, SORT_FLAG_CASE | SORT_STRING);
+		foreach ($alpha as $tmp)
+			echo $tmp . "\n";
+	}
+	if (!(empty($nb)))
+	{
+		sort($nb, SORT_STRING);
+		foreach ($nb as $tmp)
+			echo $tmp . "\n";
+	}
+	if (!(empty($rest)))
+	{
+		sort($rest, SORT_STRING);
+		foreach ($rest as $tmp)
+			echo $tmp . "\n";
+	}
 ?>
