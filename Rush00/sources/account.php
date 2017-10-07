7b2dcd0121699321d473d2734bce8d2ab3a4dfd0
Thomas JACQUET <cjacquet@e3r3p14.42.fr>
2017-10-07 15:55:34 +0200

    form connexion

---
+++ Rush00/sources/account.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>110°</title>
+		<link href="sources/index.css" rel="stylesheet" />
+		<link href="resources/icon.ico" rel="icon" />
+	</head>
+	<body>
+		<nav>
+			<a href="index.php"><img class="icon" src="resources/icon.ico" alt=""/></a>
+			<div class="links" style="border-top: 4px solid #004D40;"><a href="#">Cuisine</a></div>
+			<div class="links"><a href="#">Terrasse - Jardin</a></div>
+			<div class="links"><a href="#">Salle à manger</a></div>
+			<div class="links"><a href="#">Bureau</a></div>
+			<div class="links"><a href="#">Bar - Restaurant</a></div>
+		</nav>
+		<header>
+
+        </header>
+        <div>
+            <form method="post" action="create.php" name="index.php">
+            <fieldset>
+                <legend>Connectez-vous :</legend>    
+                Identifiant: <input type="text" name="login">
+                Mot de passe: <input type="text" name="passwd">
+            <input type="submit" name="submit" value="OK">
+            </fieldset>
+    </form>
+</div>
+	</body>
+</html>
