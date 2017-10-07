7bbb3bb58c5ffcd45bd4b90cf7b9695535510def
Mathis PANDELE <mapandel@e3r4p14.42.fr>
2017-10-07 23:19:04 +0200

    index.php 1.4 : Acceuil 75% complete

--- Rush00/sources/index.php
+++ Rush00/sources/index.php
@@ -1,27 +1,70 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<title>110°</title>
+		<title>110° Chaises pliantes</title>
 		<link href="index.css" rel="stylesheet" />
 		<link href="../resources/icon.ico" rel="icon" />
 	</head>
 	<body>
 		<nav>
 			<a href="index.php"><img class="icon" src="../resources/icon.ico" alt=""/></a>
-			<div class="links" style="border-top: 4px solid #004D40;"><a href="#">Cuisine</a></div>
-			<div class="links"><a href="#">Terrasse - Jardin</a></div>
-			<div class="links"><a href="#">Salle à manger</a></div>
-			<div class="links"><a href="#">Bureau</a></div>
-			<div class="links"><a href="#">Bar - Restaurant</a></div>
+			<div class="links" style="border-top: 4px solid #004D40;"><a href="index.php">Cuisine</a></div>
+			<div class="links"><a href="index.php">Terrasse - Jardin</a></div>
+			<div class="links"><a href="index.php">Salle à manger</a></div>
+			<div class="links"><a href="index.php">Bureau</a></div>
+			<div class="links"><a href="index.php">Bar - Restaurant</a></div>
 		</nav>
 		<header>
 			<div class="titre1">110° Chaises pliantes</div>
 			<div class="titre2">Les chaises qui se plieront à toutes vos envies</div>
 		</header>
 		<div class="body">
-			<?php
-				echo file_get_contents("connexion.html");
-			?>
+			<div class="catégorie">Accueil</div>
+			<div class="article">
+				<img class="articleimg" src="../resources/rond_gris.jpg" alt="" />
+				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+			</div>
+			<div class="article">
+				<img class="articleimg" src="../resources/bois_rembouré.jpg" alt="" />
+				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+			</div>
+			<div class="article">
+				<img class="articleimg" src="../resources/imitation_rotin.jpg" alt="" />
+				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+			</div>
+			<div class="article">
+				<img class="articleimg" src="../resources/metal_noir.jpg" alt="" />
+				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+			</div>
+			<div class="article">
+				<img class="articleimg" src="../resources/tissu_noir.jpg" alt="" />
+				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+			</div>
+			<div class="article">
+				<img class="articleimg" src="../resources/napoleon_pliante.jpg" alt="" />
+				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+			</div>
+
+			<div class="article">
+				<img class="articleimg" src="../resources/plastique_blanc.jpg" alt="" />
+				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+			</div>
+			<div class="article">
+				<img class="articleimg" src="../resources/transparent.jpg" alt="" />
+				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+			</div>
+			<div class="article">
+				<img class="articleimg" src="../resources/bois_almeria.jpg" alt="" />
+				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+			</div>
+			<div class="article">
+				<img class="articleimg" src="../resources/plastique_blanc.jpg" alt="" />
+				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+			</div>
+			<div class="article">
+				<img class="articleimg" src="../resources/metal_rembouré_gris.jpg" alt="" />
+				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+			</div>
 		</div>
 	</body>
 </html>
