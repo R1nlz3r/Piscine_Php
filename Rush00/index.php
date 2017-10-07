1ea34742a390cca1b98ae204fe815b9db6bccbae
Mathis PANDELE <mapandel@e3r4p14.42.fr>
2017-10-07 12:56:21 +0200

    index.php 1.0 : Implementation

---
+++ Rush00/index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>110°</title>
+		<link href="sources/index.css" rel="stylesheet" />
+		<link href="resources/icon.ico" rel="icon" />
+	</head>
+	<body>
+		<nav>
+			<img class="icon" src="resources/icon.ico" alt=""/>
+		</nav>
+	</body>
+</html>
