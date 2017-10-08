72cdad3fcfdd42d38576a818a8c5891b792a6530
Mathis PANDELE <mapandel@e1r1p6.42.fr>
2017-10-08 14:41:08 +0200

    index.css 1.6 : Change onglets

--- Rush00/sources/index.php
+++ Rush00/sources/index.php
@@ -11,78 +11,26 @@
 		?>
 		<nav>
 			<a href="index.php"><img class="icon" src="../resources/icon.ico" alt=""/></a>
-			<div class="links" style="border-top: 4px solid #004D40;"><a href="index.php">Cuisine</a></div>
-			<div class="links"><a href="index.php">Terrasse - Jardin</a></div>
-			<div class="links"><a href="index.php">Salle à manger</a></div>
-			<div class="links"><a href="index.php">Bureau</a></div>
-			<div class="links"><a href="index.php">Bar - Restaurant</a></div>
+			<form method="get">
+				<div class="links" style="border-top: 4px solid #004D40;"><input type="submit" name="Cuisine" value="Cuisine" /></div>
+				<div class="links"><input type="submit" name="Terrasse - Jardin" value="Terrasse - Jardin" /></div>
+				<div class="links"><input type="submit" name="Salle à manger" value="Salle à manger" /></div>
+				<div class="links"><input type="submit" name="Bureau" value="Bureau" /></div>
+				<div class="links"><input type="submit" name="Bar - Restaurant" value="Bar - Restaurant" /></div>
+			</form>
 		</nav>
 		<header>
 			<div class="titre1">110° Chaises pliantes</div>
 			<div class="titre2">Les chaises qui se plieront à toutes vos envies</div>
 		</header>
 		<div class="body">
-			<div class="catégorie">Accueil</div>
-			<?php
-					$count = 0;
-					while (($data = fgetcsv($fd, 0, ";")) !== FALSE) {
-				?>
-				<?php if ($count !== 0) { ?>
-				<div class="article">
-				<img class="articleimg" src="<?php echo $data[4];?>" alt="" />
-				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
- 				<div class="price_trick"><div class="price">/// $</div></div>
-				<div class="description_block">
-					<div class="description"> /// </div>
-				</div>
-				</div>
-			<?php
-				}
-				$count++;
-				}
-				fclose($fd);
-			?><!--
-			<div class="article">
-				<img class="articleimg" src="../resources/bois_rembouré.jpg" alt="" />
-				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
-			</div>
-			<div class="article">
-				<img class="articleimg" src="../resources/imitation_rotin.jpg" alt="" />
-				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
-			</div>
-			<div class="article">
-				<img class="articleimg" src="../resources/metal_noir.jpg" alt="" />
-				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
-			</div>
-			<div class="article">
-				<img class="articleimg" src="../resources/tissu_noir.jpg" alt="" />
-				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
-			</div>
-			<div class="article">
-				<img class="articleimg" src="../resources/napoleon_pliante.jpg" alt="" />
-				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
-			</div>
-
-			<div class="article">
-				<img class="articleimg" src="../resources/plastique_blanc.jpg" alt="" />
-				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
-			</div>
-			<div class="article">
-				<img class="articleimg" src="../resources/transparent.jpg" alt="" />
-				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
-			</div>
-			<div class="article">
-				<img class="articleimg" src="../resources/bois_almeria.jpg" alt="" />
-				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
-			</div>
-			<div class="article">
-				<img class="articleimg" src="../resources/plastique_blanc.jpg" alt="" />
-				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
-			</div>
-			<div class="article">
-				<img class="articleimg" src="../resources/metal_rembouré_gris.jpg" alt="" />
-				<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
-			</div>-->
+			<form method="get">
+				<label for="Pseudo">Pseudo</label><br />
+				<input type="text" id="Pseudo" name="pseudo"/><br />
+				<label for="Password">Mot de passe</label><br />
+				<input type="password" id="Password" name="password"/><br />
+				<input type="submit" name="submit" value="OK"/>
+			</form>
 		</div>
 	</body>
 </html>
