821fbf0e3f4ff6f0cd71c4fec7e887dc2bf63b1b
Mathis PANDELE <mapandel@e1r1p6.42.fr>
2017-10-08 20:50:29 +0200

    categories.php 1.0 : Implementation

---
+++ Rush00/sources/categories.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include("header.php");
+?>
+<div class="catégorie"><?php echo $cat[$cat_nb];?></div>
+<?php
+		$count = 0;
+		while (($data = fgetcsv($fd, 0, ";")) !== FALSE) {
+			 if ($count !== 0 && ($data[1] == $_GET['id'] || $cat_nb === 0))  { ?>
+	<div class="article">
+		<img class="articleimg" src="<?php echo $data[4];?>" alt="" />
+		<div class="add_cart_button"><div class="add_cart_text">Ajouter au panier</div></div>
+		<div class="price_trick"><div class="price"><?php echo $data[3];?> $</div></div>
+		<div class="description_block">
+			<div class="description"> <?php echo $data[0];?> :<br /> <?php echo $data[2];?></div>
+		</div>
+	</div>
+<?php
+			}
+			$count++;
+		}
+		fclose($fd);
+?>
