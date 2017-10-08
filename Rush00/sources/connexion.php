5f205d32febbdc97142af476e6e326b403df2730
Mathis PANDELE <mapandel@e1r1p6.42.fr>
2017-10-08 20:50:49 +0200

    connexion.php 1.0 : Implementation + Working

--- Rush00/sources/connexion.php
+++ Rush00/sources/connexion.php
@@ -1,11 +1,31 @@
 <?php
-	if ($_POST['submit'] === "OK" && $_POST['pseudo'] !== "" && $_POST['password'] !== "")
-	{
-		session_set();
-		$_SERVER['PHP_AUTH_USER'] = $_POST['pseudo'];
-		$_SERVER['PHP_AUTH_PW'] = $_POST['password'];
-		$_SESSION['connexion'] = 1;
-		header("Location: index.php");
+session_start();
+if ($_POST['submit'] == "OK" && $_POST['pseudo'] && $_POST['password'] != "")
+{
+	$file = file_get_contents("../data/users");
+	if ($file != "") {
+		$users = unserialize($file);
+	} else {
+		$users = array();
 	}
-	$_SESSION['connexion'] = 2;
+
+	foreach ($users as $key => $value) {
+		if ($value['pseudo'] == $_POST['pseudo'] && $value['password'] == hash("whirlpool", $_POST['password'])) {
+			$_SESSION['pseudo'] = $_POST['pseudo'];
+			header("Location: index.php");
+		}
+	}
+	$message = "Informations de connexion invalides";
+} else if ($_POST['submit'] == "OK") {
+	$message = "Veuillez renseigner tous les champs";
+}
+include("header.php");
 ?>
+<form action="connexion.php" method="post">
+	<label for="Pseudo">Pseudo</label><br />
+	<input type="text" name="pseudo"/><br />
+	<label for="Password">Mot de passe</label><br />
+	<input type="password" name="password"/><br />
+	<?= $message.'<br />'; ?>
+	<input type="submit" name="submit" value="OK"/>
+</form>
