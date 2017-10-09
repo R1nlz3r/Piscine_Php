15698c626748fe9c693016cecec31f4eadcd62d0
Mathis Pandelé <mapandel@student.42.fr>
2017-10-09 15:35:47 +0200

    ex01.sql : Implementation

---
+++ Day05/ex01/ex01.sql
@@ -0,0 +1,7 @@
+CREATE TABLE ft_table
+(
+	id INT PRIMARY KEY NOT NULL,
+	login TEXT VARCHAR(8) DEFAULT toto NOT NULL,
+	groupe ENUM('STAFF', 'STUDENT', 'OTHER') NOT NULL,
+	date_de_creation DATE NOT NULL,
+)
