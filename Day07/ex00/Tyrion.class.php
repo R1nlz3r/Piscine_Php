d2f6e2d040a86f7e3b909a6aaa796fe518a31207
Mathis PANDELE <mapandel@e1r13p9.42.fr>
2017-10-11 18:24:58 +0200

    Tyrion.class.php 1.0 : Implementation

---
+++ Day07/ex00/Tyrion.class.php
@@ -0,0 +1,21 @@
+<?php
+class Tyrion extends Lannister
+{
+	public function __construct()
+	{
+		parent::__construct();
+		print("My name is Tyrion" . PHP_EOL);
+		return ;
+	}
+
+	public function getSize()
+	{
+		return "Short";
+	}
+
+	public function __destruct()
+	{
+		return ;
+	}
+}
+?>
